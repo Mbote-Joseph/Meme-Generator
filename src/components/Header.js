d0bd3215ea7b2d04dfd6de159419597d39fcf03f
Mbote-Joseph <mbotejoseph001@gmail.com>
2023-01-10 21:15:52 +0300

    Meme Generator using React

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,16 @@
-import trollImage from './Troll.png'
-import './Header.css'
+import trollImage from "./Troll.png";
+import "./Header.css";
 
-let Header=()=>{
-    return(
-        <header>
-        <div className="image">
+let Header = () => {
+  return (
+    <header>
+      <div className="image">
         <img src={trollImage} alt="_blank" />
         <h2>Meme Generator</h2>
-        </div>
-        <h4>Mbote Joseph</h4>
-
-        </header>
-    )
-}
+      </div>
+      <h4>Mbote Joseph</h4>
+    </header>
+  );
+};
 
 export default Header;
